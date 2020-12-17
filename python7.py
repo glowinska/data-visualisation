7f5f5ab8fb33a0ec4906661cc9893c959185926c
Anna <anna.glowinska98@gmail.com>
2020-12-17 23:58:42 +0100

    table evolution

--- python7.py
+++ python7.py
@@ -3,6 +3,8 @@ import dash_core_components as dcc
 import dash_html_components as html
 import pandas as pd
 import json
+from dash.dependencies import Input, Output
+import dash_table
 
 df = pd.read_json('link', orient="records")
 print(df)
@@ -11,17 +13,27 @@ print(df.head())
 s = df.iloc[0]
 print(s[0])
 
-def generate_table(dataframe, max_rows=10):
-    return html.Table(
-        # Header
-        [html.Tr([html.Th(col) for col in s[0].keys()])] +
+PAGE_SIZE = 1
 
-        # Body
-        [html.Tr([
-            html.Td(dataframe.iloc[i][0][col]) for col in s[0].keys()
-        ]) for i in range(min(len(dataframe), max_rows))]
+def generate_table(dataframe, max_rows=10):
+    return dash_table.DataTable(
+    id='datatable-paging',
+    columns=[
+        {"name": i, "id": i} for i in sorted(df.columns)
+    ],
+    page_current=0,
+    page_size=PAGE_SIZE,
+    page_action='custom'
     )
 
+@app.callback(
+    Output('datatable-paging', 'data'),
+    Input('datatable-paging', "page_current"),
+    Input('datatable-paging', "page_size"))
+def update_table(page_current,page_size):
+    return df.iloc[
+        page_current*page_size:(page_current+ 1)*page_size
+    ].to_dict('records')
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
@@ -33,4 +45,4 @@ app.layout = html.Div(children=[
 ])
 
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    app.run_server()
